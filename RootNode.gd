bb123c80b0cbe2916cb92ff1b6663d8ee2dd8cd5
907riley <907riley@gmail.com>
2023-10-19 20:24:08 -0700

    got head tracking working more or less lmao

--- RootNode.gd
+++ RootNode.gd
@@ -4,20 +4,30 @@ extends Node3D
 
 var mouse_position_3d: Vector3
 
+# head
+var head: Transform3D
+var head_id: int
+
+# hands
 var left_hand: Transform3D
 var left_hand_id: int
 
 var right_hand: Transform3D
 var right_hand_id: int 
 
+# arms
 var left_forearm: Transform3D
 var left_forearm_id: int
 
 var right_forearm: Transform3D
 var right_forearm_id: int
 
+# markers for hands
 var left_ik: Marker3D
 var right_ik: Marker3D
+
+# markers for head
+var head_marker: Marker3D
 
 @export var hand_separation: float = .1
 
@@ -38,6 +48,10 @@ func _ready():
 	skeleton.set_bone_pose_rotation(left_forearm_id, Quaternion(0, -1, 0, 1))
 	skeleton.set_bone_pose_rotation(right_forearm_id, Quaternion(0, 1, 0, 1))	
 	
+	head_id = skeleton.find_bone("mixamorig_Head")
+	head = skeleton.get_bone_global_pose(head_id)
+	
+	head_marker = $'Skeleton3D/Eyes'
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -50,7 +64,13 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	mouse_position_3d = Vector3(position.x, position.y, 0)
 	var mouse_position_left: Vector3 = Vector3(mouse_position_3d.x + hand_separation, mouse_position_3d.y, 0)
 	var mouse_position_right: Vector3 = Vector3(mouse_position_3d.x - hand_separation, mouse_position_3d.y, 0)	
+	
 	if not Input.is_action_pressed("hold_left"):
 		left_ik.set_position(mouse_position_left)
 	if not Input.is_action_pressed("hold_right"):		
 		right_ik.set_position(mouse_position_right)	
+		
+	head = head.looking_at(mouse_position_3d, Vector3(0, 1, 0), true)
+#	head = head.orthonormalized()
+#	head.basis = head.basis.rotated(head.basis.x, deg_to_rad(5))
+	skeleton.set_bone_pose_rotation(head_id, head.basis)
